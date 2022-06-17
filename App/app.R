72dc1c2331eab5052e4e93431e52ea76d0b080c4
msara <madhi.saranadasa@gmail.com>
2022-06-17 16:34:01 -0400

    Leaflet Plotting

--- App/app.R
+++ App/app.R
@@ -1,41 +1,82 @@
-
 library(shiny)
+library(leaflet)
+library(tidyverse)
 
-# Define UI for application that draws a histogram
+projectfolder <- dirname(getwd())
+
+datapath <- file.path(projectfolder, "Data", "CTD_DetailsClean.csv")
+CTD_DetailsClean <- read_csv(datapath)
+
+datapath <- file.path(projectfolder, "Data", "CTD_LocationAdded.csv")
+CTD_LocationAdded <- read_csv(datapath)
+
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  theme = bslib::bs_theme(bootswatch = "slate"),
+  
+  navbarPage("NCFB Clinical Trials Dashboard",
+             tabPanel(icon("home")),
+             tabPanel("Endpoint Explorer"),
+             tabPanel("About this dashboard"),
+  ),
+    
+  splitLayout(
+    
+    mainPanel(
+      sliderInput(inputId = "year", label = "Year:",
+                  min = 2000, max = 2022, step = 1, value = 2000, width = "100vh", sep = ""),
+      
+      leafletOutput(outputId = "mymap",
+                    height="70vh", width = "90vh")
+    ),
+    
+    mainPanel(
+      tableOutput(outputId = "stats")
     )
+  
+  )
+
 )
+  
 
-# Define server logic required to draw a histogram
+
+
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+  
+  output$mymap <- renderLeaflet({
+    
+    study_list <- CTD_DetailsClean %>%
+      filter(year(start_date) == input$year) %>%
+      distinct(id, enrollment) 
+    
+    locations <- CTD_LocationAdded %>%
+      inner_join(study_list, by = "id")
+    
+    basemap <- leaflet(data = locations) %>%
+      addTiles() %>%
+      setView(-96, 37.8, 4) %>%
+      addCircles(lng = ~lon,
+                 lat = ~lat,
+                 weight = 1,
+                 radius = ~enrollment * 100)
+    
+    basemap
+    
+  })
+  
+  output$stats <- renderTable({
+    
+    study_list <- CTD_DetailsClean %>%
+      filter(year(start_date) == input$year) %>%
+      distinct(id) %>%
+      pull(id)
+    
+    locations <- CTD_LocationAdded %>%
+      filter(id %in% study_list)
+  
+  })
+  
 }
 
-# Run the application 
+
 shinyApp(ui = ui, server = server)
