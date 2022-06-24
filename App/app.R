46c03a18207f44fbb8e6e91f0d3e265507706c30
msara <madhi.saranadasa@gmail.com>
2022-06-23 20:05:37 -0400

    Clickable year graph

--- App/app.R
+++ App/app.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(lubridate)
 library(htmltools)
 library(RColorBrewer)
+library(thematic)
 
 markercolors <- c('red', 'darkred', 'orange', 'green', 'darkgreen', 'blue', 'purple', 'darkpurple', 'cadetblue')
 
@@ -15,58 +16,62 @@ CTD_DetailsClean <- read_csv(datapath) %>%
 
 datapath <- file.path(projectfolder, "Data", "CTD_LocationAdded.csv")
 CTD_LocationAdded <- read_csv(datapath) 
-  
+
+thematic_shiny(font = "auto")
 
 ui <- fluidPage(
 
   theme = bslib::bs_theme(bootswatch = "slate"),
   
+  
   navbarPage("NCFB Clinical Trials Dashboard",
-             tabPanel(icon("home")),
-             tabPanel("Endpoint Explorer"),
+             tabPanel("Explore by geography"),
+             tabPanel("Explore by endpoint"),
              tabPanel("About this dashboard"),
   ),
     
-  splitLayout(
+  fluidRow(
     
-    wellPanel(width = 8,
-      
-      fluidRow(
-        p("Scrub through years and select trial of interest:")
-      ),
-      
-      fluidRow(
-        column(width = 12, 
-               sliderInput(inputId = "year", label = "Year:",
-                           min = 2000, max = 2022, step = 1, value = 2000, width = "100vh", sep = "", animate = TRUE)
-               )
-      ),
-      fluidRow(
-        column(width = 12,
-               leafletOutput(outputId = "mymap", height="70vh", width = "90vh")
-               )
-      )
-    ),
+    column(width = 6,
+           wellPanel(
+             
+             tags$h4("Number of studies started in each year", align = "center"),
+             
+             plotOutput(outputId = "yearPlot", 
+                        click = "yearPlot_click",
+                        height="10vh"),
+             
+             br(),
+             br(),
+             
+             
+             
+             fluidRow(column(width = 10, offset = 1, sliderInput(inputId = "year", 
+                                                     label = "",
+                                                     min = 2004, 
+                                                     max = 2021, 
+                                                     step = 1, 
+                                                     value = 2004,
+                                                     sep = "", 
+                                                     animate = TRUE,
+                                                     width = "80vh")), align = "center"),
+             
+             leafletOutput(outputId = "mymap")
+             
+             )
+           ),
     
-    mainPanel(width = 12, 
-      fluidRow(
-        column(width = 12, align = "center", 
-               textOutput(outputId = "rightside")
-        )
-      ),
-      
-      fluidRow(align = "center",
-        column(width = 8, align = "center",
-               leafletOutput(outputId = "studymap"))
-      )
-    )
-    
+    column(width = 6, 
+           
+           tags$h4("Details for the selected study", align = "center"),
+           htmlOutput(outputId = "titleblock")
+           )
     
   )
 )
   
 
-server <- function(input, output) {
+server <- function(input, output, session) {
   
   # ----- observe for map updating -----
   observe({
@@ -112,6 +117,19 @@ server <- function(input, output) {
   })
   
   
+  # ----- reactive to subset study year on click -----
+  onYearPlotClick <- observe({
+    test = input$yearPlot_click$x
+    
+    if(is.null(test))
+      return()
+    
+    updateSliderInput(session, "year", value = round(test))
+    
+    round(test)
+  })
+  
+  
   # ----- reactive to fetch study details on click -----
   onClickGetStudy <- reactive({
     click<-input$mymap_marker_click
@@ -125,13 +143,53 @@ server <- function(input, output) {
       inner_join(CTD_DetailsClean, by = "id")
   })
   
+
+  # ----- render yearly plot -----
+  output$yearPlot <- renderPlot({
+    
+    test <- CTD_DetailsClean %>%
+      mutate(year = year(start_date)) %>%
+      group_by(year) %>%
+      summarise(n = n())
+    
+    ggplot() +
+      geom_line(data = test,
+                aes(x = year,
+                    y = n),
+                size = 0.75) +
+      scale_y_continuous(limits = c(0, NA),
+                         breaks = c(0, 10)) +
+      scale_x_continuous(limits = c(2004, 2021),
+                         breaks = seq(2004, 2021, 1)) +
+      theme(axis.title.x = element_blank(),
+            axis.title.y = element_blank(),
+            axis.text = element_text(size = 12),
+            axis.ticks = element_blank())
+  })
   
-  # ----- render right side -----
-  output$rightside <- renderText({
+  
+  # ----- render title -----
+  output$title <- renderText({
     
     study <- onClickGetStudy()
     text<-study$title
     
+  })
+  
+  
+  # ----- render title -----
+  output$titleblock <- renderUI({
+    study <- onClickGetStudy()
+    
+    if(is.null(study))
+      return()
+    
+    tags$body(
+      tags$h3(study$title, align = "center"),
+      tags$h4(study$phase, align = "center"),
+      tags$h4("Sponsor:", study$agency, align = "center"),
+      tags$p(study$brief_summary)
+    )
   })
   
   
@@ -145,18 +203,7 @@ server <- function(input, output) {
     basemap
     
   })
-  
-  # ----- render the study map -----
-  output$studymap <- renderLeaflet({
-    
-    basemap <- leaflet() %>%
-      addTiles() %>%
-      setView(-96, 37.8, 4) 
-    
-    basemap
-    
-  })
-  
+
 }
 
 
